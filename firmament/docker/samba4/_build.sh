82492e955b846c97f3d4b6de6ac602dd037a0c55
John <john.reeme@gmail.com>
2018-05-17 13:42:10 -0500

    Removing 'joshua' & 'samba4'

--- firmament/docker/samba4/_build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker image build -t 52.0.211.45:5000/merlin-samba4:latest -t 10.1.70.193:5000/merlin-samba4:latest .
-
--- firmament/docker/samba4/_run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run -dt -v $PWD/smb.conf:/etc/samba/smb.conf -p 445:445 --name merlin-samba4 --restart=always 52.0.211.45:5000/merlin-samba4
