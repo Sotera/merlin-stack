c2057cd2833496947d479407e55cc1fe2c2c48ce
John <john.reeme@gmail.com>
2018-08-01 15:26:00 -0500

    Optimize 'image' tags in deploy JSON

--- firmament/docker/hadoop-base-alpine/_build.sh
+++ firmament/docker/hadoop-base-alpine/_build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --rm -t 52.0.211.45:5000/merlin-hadoop-base-alpine:0.07.13 .
+docker build --rm -t 52.0.211.45:5000/merlin-hadoop-base-alpine:0.07.14 -t docker-registry.parrot.keyw:5000/merlin-hadoop-base-alpine:0.07.14 -t docker-registry.parrot-rp.keyw:5000/merlin-hadoop-base-alpine:0.07.14 -t docker-registry.parrot-les.keyw:5000/merlin-hadoop-base-alpine:0.07.14 .
