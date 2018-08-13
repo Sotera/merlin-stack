bef15603dc585766a24d64381e5541342dea11fd
jstnldrs <justin.lueders@gmail.com>
2018-08-13 11:23:31 -0500

    update classpath.text with merlin dirs

---
+++ firmament/docker/hadoop-base-alpine/config/update_classpath.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+CLASSPATH_FILE=/usr/local/spark/conf/classpath.txt
+
+echo /mnt/merlin/merlin-etl/share/libs/cubes/* | cat - $CLASSPATH_FILE > temp && mv temp $CLASSPATH_FILE
+echo /mnt/merlin/merlin-etl/share/libs/common/* | cat - $CLASSPATH_FILE > temp && mv temp $CLASSPATH_FILE
+
+exit
