30cb3bcd953ef9ffc905107a468f8569796eba8f
Robert <rvemble@gmail.com>
2018-08-22 07:53:39 -0500

    added spacy to requirements; added tika and maxmind directories to classpath

--- firmament/docker/hadoop-base-alpine/config/update_classpath.sh
+++ firmament/docker/hadoop-base-alpine/config/update_classpath.sh
@@ -4,5 +4,7 @@ CLASSPATH_FILE=/usr/local/spark/conf/classpath.txt
 
 echo /mnt/merlin/merlin-etl/share/libs/cubes/* | cat - $CLASSPATH_FILE > temp && mv temp $CLASSPATH_FILE
 echo /mnt/merlin/merlin-etl/share/libs/common/* | cat - $CLASSPATH_FILE > temp && mv temp $CLASSPATH_FILE
+echo /mnt/merlin/merlin-etl/share/libs/common/tika/* | cat - $CLASSPATH_FILE > temp && mv temp $CLASSPATH_FILE
+echo /mnt/merlin/merlin-etl/share/libs/common/maxmind/* | cat - $CLASSPATH_FILE > temp && mv temp $CLASSPATH_FILE
 
 exit
