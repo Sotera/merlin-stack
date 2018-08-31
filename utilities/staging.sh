1ced7e797b6914bdb278721dc2caba33a553c9fa
Robert <rvemble@gmail.com>
2018-08-31 06:13:06 -0500

    adding useful scripts - deploy stack, connect to staging

---
+++ utilities/staging.sh
@@ -0,0 +1,12 @@
+﻿﻿#!/usr/bin/env bash
+
+
+STACK_NAME="full-stack-merlin"
+
+MACHINE_NAME="$1"
+if [ -z "$MACHINE_NAME" ]; then
+  MACHINE_NAME=$(docker-machine ls | grep stagingServer | awk '{print$1}') 
+fi
+
+eval $(docker-machine env $MACHINE_NAME)
+docker exec -e COLUMNS="`tput cols`" -e LINES="`tput lines`" -it $(docker ps | grep 'stagingserver' | awk '{print $1}') /bin/bash
