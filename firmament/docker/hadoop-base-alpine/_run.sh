babca0e5fd88ae8120334a7b56704d239b055e8b
John <john.reeme@gmail.com>
2018-09-17 12:30:28 -0500

    Hopefully a good production deploy for parrot-scif

--- firmament/docker/hadoop-base-alpine/_run.sh
+++ firmament/docker/hadoop-base-alpine/_run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -dt --name merlin-hadoop-base-alpine -h namenode -e HADOOP_MACHINE_ROLE=namenode 52.0.211.45:5000/merlin-hadoop-base-alpine:0.07.15
+docker run -dt --name merlin-hadoop-base-alpine -h namenode -e HADOOP_MACHINE_ROLE=namenode 52.0.211.45:5000/merlin-hadoop-base-alpine:0.07.16
