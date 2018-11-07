152dd0aeb8cfbe12624c83565f538bc65114692c
John <john.reeme@gmail.com>
2018-11-07 14:20:31 -0600

    WIP to base merlin-stack/hadoop on python-jre image

---
+++ firmament/docker/hadoop/_build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build --rm -t docker-registry.parrot-les.keyw:5000/merlin-hadoop-base-alpine:0.08.01 .
--- firmament/docker/newman-phusion/4.0.0/_run.sh
+++ firmament/docker/newman-phusion/4.0.0/_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -dt --name newman -h newman -p 5000:5000 52.0.211.45:5000/newman:0.07.19
+docker run -dt --name newman -h newman -p 5000:5000 docker-registry.parrot-les.keyw:5000/newman:0.08.01
 
