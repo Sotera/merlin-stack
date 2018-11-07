045e6b9f893bddc94a17a0b709f205f7c53ab41f
jstnldrs <justin.lueders@gmail.com>
2018-11-07 10:58:06 -0600

    move to v 19

--- firmament/docker/newman-phusion/4.0.0/_build.sh
+++ firmament/docker/newman-phusion/4.0.0/_build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --rm -t 52.0.211.45:5000/newman:0.07.17 .
+docker build --rm -t 52.0.211.45:5000/newman:0.07.19 -t 10.1.70.193:5000/newman:0.07.19 --no-cache .
--- firmament/docker/newman-phusion/4.0.0/_run.sh
+++ firmament/docker/newman-phusion/4.0.0/_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -dt --name newman -h newman -p 5000:5000 52.0.211.45:5000/newman:0.07.17
+docker run -dt --name newman -h newman -p 5000:5000 52.0.211.45:5000/newman:0.07.19
 
