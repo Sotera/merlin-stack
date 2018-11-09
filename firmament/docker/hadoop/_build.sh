dc1aabc62b058efad72324758f19914356cd877a
John <john.reeme@gmail.com>
2018-11-09 12:00:12 -0600

    Stashing to check out master

--- firmament/docker/hadoop/_build.sh
+++ firmament/docker/hadoop/_build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --rm -t docker-registry.parrot-les.keyw:5000/merlin-hadoop-base-alpine:0.08.01 .
+docker build --rm -t docker-registry.parrot-les.keyw:5000/merlin-hadoop:0.08.01 .
--- firmament/docker/hadoop/_run.sh
+++ firmament/docker/hadoop/_run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -dt --name merlin-hadoop-base-alpine -h namenode -e HADOOP_MACHINE_ROLE=namenode docker-registry.parrot-les.keyw:5000/merlin-hadoop-base-alpine:0.08.01
+docker run -dt --name merlin-hadoop -h namenode -e HADOOP_MACHINE_ROLE=namenode docker-registry.parrot-les.keyw:5000/merlin-hadoop:0.08.01
