29561fc977b761fa7d0ff56eea7e8b03bcb03941
John <john.reeme@gmail.com>
2019-01-04 10:04:18 -0600

    OK, finished up the stack refactor

--- firmament/docker/kibana/5.6/_build.sh
+++ firmament/docker/kibana/5.6/_build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --rm -t 52.0.211.45:5000/kibana:0.08.04 .
+docker build --no-cache --rm -t 192.168.99.100:5000/kibana:0.08.05 .
--- firmament/docker/nifi/_build.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker build --rm -t 192.168.99.100:5000/nifi:0.08.05 .
