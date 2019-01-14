013afe8053118c3b3032030f79a122d7f37f714d
John <john.reeme@gmail.com>
2019-01-14 15:35:32 -0600

    Modify run scripts for containers that use volumes so that permissions are set correctly!

--- firmament/docker/kibana/5.6/_run.sh
+++ firmament/docker/kibana/5.6/_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -dt --name nifi -h nifi -p 2222:22 docker-registry.parrot-scif.keyw:5000/nifi:0.08.05
+docker run -dt --name nifi -h nifi -p 2222:22 docker-registry.parrot-scif.keyw:5000/nifi:0.08.06
 
