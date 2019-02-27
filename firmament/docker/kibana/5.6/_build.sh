30f7c462e463e89e0ea21d238147e7a6fc5b804c
John <john.reeme@gmail.com>
2019-02-27 15:06:10 -0600

    More gov cloud

--- firmament/docker/kibana/5.6/_build.sh
+++ firmament/docker/kibana/5.6/_build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --rm -t docker-registry.parrot-scif.keyw:5000/kibana:0.08.06 .
+docker build --rm -t docker-registry.parrot-scif.keyw:5000/kibana:0.08.06 -t 52.0.211.45:5000/kibana:0.08.06 -t 10.1.70.193:5000/kibana:0.08.06 .
